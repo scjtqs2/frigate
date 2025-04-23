e97dd363b92b29a600dfef378e44629cd3895efb
root <root@truenas-x11>
2025-04-23 23:15:04 +0800

    build

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/zsh
+VERSION="0.16.0"
+COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+echo "VERSION = \"${VERSION}-${COMMIT_HASH}\"" > frigate/version.py
+IMAGE_TAG="ghcr.io/blakeblackshear/frigate:0.15.1"
 
-COMMIT_HASH=$(git log -1 --pretty=format:"%h"|tail -1)
-VERSION="dg1"
-IMAGE_REPO="scjtqs/frigate"
-GITHUB_REF_NAME=$(git rev-parse --abbrev-ref HEAD)
-docker buildx create --name multiarch-builder --use
-docker buildx build --file docker/main/Dockerfile . \
-    --tag ${IMAGE_REPO}:${GITHUB_REF_NAME}-${COMMIT_HASH} \
-    --platform linux/amd64 \
-    --push
-docker buildx rm multiarch-builder
+docker build -t "${IMAGE_TAG}" -f docker/main/Dockerfile .
+#docker push "${IMAGE_TAG}"
