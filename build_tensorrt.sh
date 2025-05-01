95a080d7c40da5db92db7e4409bd1cbba3d11f9c
scjtqs2 <scjtqs@me.com>
2025-05-02 01:45:11 +0800

    update

--- build_tensorrt.sh
+++ build_tensorrt.sh
@@ -4,9 +4,22 @@ COMMIT_HASH=$(git log -1 --pretty=format:"%h")
 echo "VERSION = \"${VERSION}-${COMMIT_HASH}\"" > frigate/version.py
 IMAGE_TAG="ghcr.io/blakeblackshear/frigate:0.15.1-tensorrt"
 
-docker build -t base -f docker/main/Dockerfile .
-docker build -t tensorrt-base \
---build-arg  COMPUTE_LEVEL="50 60 70 80 90" \
- -f docker/tensorrt/Dockerfile.base .
-docker build -t "${IMAGE_TAG}" -f docker/tensorrt/Dockerfile.amd64 .
+#docker build -t base -f docker/main/Dockerfile .
+#docker build -t tensorrt-base \
+#--build-arg  COMPUTE_LEVEL="50 60 70 80 90" \
+# -f docker/tensorrt/Dockerfile.base .
+#docker build -t "${IMAGE_TAG}" -f docker/tensorrt/Dockerfile.amd64 .
 #docker push "${IMAGE_TAG}"
+# 启用 Buildx
+docker buildx create --use --name frigate-builder
+docker buildx inspect --bootstrap
+
+# 构建 TensorRT 镜像（AMD64）
+docker buildx bake \
+  -f docker/tensorrt/trt.hcl \
+  --set "*.platforms=linux/amd64" \
+  --set "*.args.COMPUTE_LEVEL=50 60 70 80 90" \
+  --set "*.args.TRT_BASE=nvidia/cuda:12.2-base" \  # 根据需求修改
+  --set "tensorrt.tags=${IMAGE_TAG}"
+  --load \        # 将镜像加载到本地
+  target:tensorrt # 指定构建目标
