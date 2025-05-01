89190bbe2166c456a34d805cdd293b6de84a9cd3
scjtqs2 <scjtqs@me.com>
2025-05-02 01:24:44 +0800

    update

--- build_tensorrt.sh
+++ build_tensorrt.sh
@@ -4,5 +4,6 @@ COMMIT_HASH=$(git log -1 --pretty=format:"%h")
 echo "VERSION = \"${VERSION}-${COMMIT_HASH}\"" > frigate/version.py
 IMAGE_TAG="ghcr.io/blakeblackshear/frigate:0.15.1-tensorrt"
 
-docker build -t "${IMAGE_TAG}" -f docker/tensorrt/trt.hcl .
+export COMPUTE_LEVEL="50 60 70 80 90" && docker build -t tensorrt-base -f docker/tensorrt/Dockerfile.base .
+docker build -t "${IMAGE_TAG}" -f docker/tensorrt/Dockerfile.amd64 .
 #docker push "${IMAGE_TAG}"
